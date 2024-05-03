ecfb03e050cfda58a5c05c1ef6100a9e367aa5b1
Emile Junior ADA <junada99@gmail.com>
2024-05-03 23:36:23 +0200

    Adding Home Particles

---
+++ components/Home/Particles/HomeParticles.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+//import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+const HomeParticles = () => {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadFull(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        links: {
+          color: "#fff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 40,
+        },
+        opacity: {
+          value: 0.2,
+        },
+        shape: {
+          type: "image",
+          close: true,
+          fill: true,
+          options: {
+            image: [
+              {
+                src: "/particles/vue.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/c.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/c++.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/css.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/html.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/react.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/js.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+              {
+                src: "/particles/python.png",
+                gif: false,
+                height: 32,
+                width: 32,
+              },
+            ],
+          },
+        },
+        size: {
+          value: 15,
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+        className="flex"
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default HomeParticles;
