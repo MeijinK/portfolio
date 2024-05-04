3c3e7fd453fb148e51406f80ec145f520814ab75
Emile Junior ADA <38108312+MeijinK@users.noreply.github.com>
2024-05-04 09:58:58 +0200

    Update GetInTouch.tsx

--- components/Home/GetInTouch/GetInTouch.tsx
+++ components/Home/GetInTouch/GetInTouch.tsx
@@ -5,7 +5,7 @@ export default function GetInTouch() {
     <div
       id="GetInTouchSection"
       data-aos="fade-up"
-      className="flex flex-col space-y-4 w-full h-96 items-center bg-AAprimary"
+      className="flex flex-col space-y-4 w-full px-4 h-96 items-center bg-AAprimary"
     >
       {/* // ? Title === > What's Next?  */}
       <div className="flex flex-row items-center ">
@@ -14,7 +14,7 @@ export default function GetInTouch() {
         </span>
       </div>
       {/* // ? Get In Touch */}
-      <span className="text-AAsecondary text-3xl sm:text-4xl px-4 font-bold tracking-wider opacity-85">
+      <span className="text-AAsecondary text-3xl sm:text-4xl font-bold tracking-wider opacity-85">
         <span className="uppercase">ç</span>a tombe bien j&apos;aime vous lire
       </span>
       <p className="flex font-Header tracking-wider text-AATextAccent/70 text-center px-6 sm:px-16 md:px-0 md:w-[600px]">
@@ -22,14 +22,14 @@ export default function GetInTouch() {
         partager ou que vous ayez des questions. Je suis disponible via ma boîte
         mail et je réponds dans la minute.
       </p>
-      <div className="pt-4 px-4">
+      <div className="pt-4">
         <a
           href="mailto:emilejuniorada@yahoo.com"
           target={"_blank"}
           rel="noreferrer"
         >
           <button
-            className="font-mono text-sm border-AATextAccent
+            className="font-mono text-sm border-AATextAccent mx-2
            hover:bg-AATextAccent hover:text-white text-AATextAccent px-8 py-4 border-[1.5px] rounded-full "
           >
             Faites l&apos;essai si vous ne me croyez pas
