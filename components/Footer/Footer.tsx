0e38db0e5799f7748efece2573559db548e44e85
Emile Junior ADA <38108312+MeijinK@users.noreply.github.com>
2024-05-04 10:02:37 +0200

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -31,21 +31,6 @@ export default function Fotter(props: {
   return (
     <div className="bg-AAprimary flex flex-col justify-center items-center py-8 space-y-4">
       {/* // ? Reach me at */}
-      <div
-        className={`flex flex-row space-x-8 ${
-          props.hideSocialsInDesktop ? "lg:hidden" : ""
-        }`}
-      >
-        {IconsData.map((iconData, index) => {
-          return (
-            <ClickableIcon
-              key={index}
-              href={iconData.href}
-              Icon={iconData.Icon}
-            />
-          );
-        })}
-      </div>
       <a href={props.myUrl} className="" target={"_blank"} rel="noreferrer">
         <div
           className="group flex flex-col font-mono justify-center items-center  text-AATextAccent/70
